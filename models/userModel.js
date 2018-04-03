65b3f83c2fba1ea52e0bb1810ea4b44e12c189e4
dorianSTEM <31543262+dorianSTEM@users.noreply.github.com>
2018-04-03 08:43:19 -0600

    Add files via upload

--- models/userModel.js
+++ models/userModel.js
@@ -5,14 +5,38 @@ modelHelper.mongo.onReady = function(){
     db = modelHelper.mongo.users
 }
 
-exports.findUser = function(username, password){
-    return new Promise(function(resolveParent){
+exports.findUser = function(username, password){ // Find User by username and password (for authentication)
+    return new Promise(function(resolve){
         db.findOne({username:username, password:password}).then(function(doc) {
-            if (doc) {
-                resolveParent(true);
-            } else {
-                resolveParent(false);
-            }
+          if (doc) {
+              resolve(true);
+          } else {
+              resolve(false);
+          }
         });
     });
 }
+
+exports.findUserByUsername = function(username){ // Find User by username (to make sure user is unique)
+  return new Promise(function(resolve){
+    db.findOne({username:username}).then(function(doc) {
+        if (doc) {
+            resolve(true);
+        } else {
+            resolve(false);
+        }
+    });
+  });
+}
+
+exports.createUser = function(username, password){
+  return new Promise(function(resolve){
+    db.insertOne({username:username, password:password}, function(err, doc) {
+        if (!err) {
+            resolve(true);
+        } else {
+            resolve(false);
+        }
+    });
+  });
+};
