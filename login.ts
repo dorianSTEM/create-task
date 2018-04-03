4a9ae7f7a3b9256a3ac7fba3327332f9dd97ee67
dorianSTEM <31543262+dorianSTEM@users.noreply.github.com>
2018-04-03 08:20:55 -0600

    Add files via upload

---
+++ login.ts
@@ -0,0 +1,87 @@
+import { Http } from '@angular/http';
+
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { ToastController } from 'ionic-angular';
+import { LoadingController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-login',
+  templateUrl: 'login.html'
+})
+export class LoginPage {
+  creds:any = {};
+  constructor(
+    public navCtrl: NavController,
+    private http: Http,
+    public toastCtrl: ToastController,
+    public loadingCtrl: LoadingController
+  ) { }
+
+  login() { // Login Authentication function with server
+    let data = {
+      usr: this.creds.username, // the data that will be sent to the server
+      pwd: this.creds.password // field names are shortened to reduce traffic
+    }
+
+    let loading = this.loadingCtrl.create({
+      content: 'Authenticating...'
+    });
+
+    loading.present();
+
+    this.http.post('/login', data).subscribe(response => {
+        var resBody = JSON.parse(response["_body"]);
+
+      var toastMsg = "";
+      if (resBody.loggedIn){
+        toastMsg = "Successfully Logged In!"
+      } else {
+        toastMsg = "Invalid Credentials"
+      }
+      let toast = this.toastCtrl.create({
+        message: toastMsg,
+        duration: 2000
+      });
+      loading.dismiss();
+      toast.present();
+    });
+  }
+  
+  register() {
+    let data = {
+      usr: this.creds.username, // the data that will be sent to the server
+      pwd: this.creds.password // field names are shortened to reduce traffic
+    }
+    
+    let loading = this.loadingCtrl.create({
+      content: 'Creating Account...'
+    });
+    
+    loading.present();
+    
+    this.http.post('/signup', data).subscribe(response => {
+      var resBody = JSON.parse(response["_body"]);
+      loading.dismiss();
+      
+      if (!resBody.err) {
+        let toast = this.toastCtrl.create({
+          message: "Successfully Registered!",
+          duration: 2000
+        });
+
+        toast.present();
+      } else {
+        let toast = this.toastCtrl.create({
+          message: "Error, please try again.",
+          duration: 2000
+        });
+
+        toast.present();
+      }
+    });
+  }
+}
+
+
+// https://ionicframework.com/docs/native/toast/
