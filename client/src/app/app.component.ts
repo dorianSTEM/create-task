1165ddd562c6b429ef9a8e68a19512e8c6bab31d
dorianSTEM <31543262+dorianSTEM@users.noreply.github.com>
2018-04-05 08:56:31 -0600

    Add files via upload

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,7 +1,9 @@
+import { Http } from '@angular/http';
 import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { Storage } from '@ionic/storage';
 
 import { HomePage } from '../pages/home/home';
 import { LoginPage } from '../pages/login/login';
@@ -12,12 +14,26 @@ import { LoginPage } from '../pages/login/login';
 export class MyApp {
   rootPage:any = LoginPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, http: Http, storage: Storage) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
-      statusBar.styleDefault();
-      splashScreen.hide();
+      
+      storage.get('session-id').then((val) => {
+        
+        console.log('We have session ', val);
+        if (val){
+          this.http.post('/authenticate', data).subscribe(response => {
+            var resBody = JSON.parse(response["_body"]);
+            if (!resBody.err){
+              this.rootPage = HomePage; 
+            }
+          });
+        }
+      }).then(() = > {
+          statusBar.styleDefault();
+          splashScreen.hide();
+      });
     });
   }
 }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 import { HttpModule } from '@angular/http';
+import { IonicStorageModule } from '@ionic/storage';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -20,6 +21,7 @@ import { LoginPage } from '../pages/login/login';
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot(),
     HttpModule,
     //Toast
   ],
