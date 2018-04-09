ea89c0a528aaaa52c246ffbaa2fe9c89630dabd6
dorianSTEM <31543262+dorianSTEM@users.noreply.github.com>
2018-04-09 09:29:13 -0600

    Add files via upload

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,7 +2,6 @@ import { Http } from '@angular/http';
 import { Component, ViewChild } from '@angular/core';
 import { Nav, Platform } from 'ionic-angular';
 
-import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { Storage } from '@ionic/storage';
