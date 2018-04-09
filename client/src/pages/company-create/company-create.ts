0bf43cce3fedb914f4ce3a1b69b5e999375aa8ff
dorianSTEM <31543262+dorianSTEM@users.noreply.github.com>
2018-04-09 08:37:04 -0600

    Add files via upload

--- client/src/pages/company-create/company-create.ts
+++ client/src/pages/company-create/company-create.ts
@@ -9,6 +9,42 @@ export class CompanyCreatePage {
   creds:any = {};
   
   constructor(public navCtrl: NavController) {
+    
+  }
+  
+  createComp() {
+    let data = {
+      //usr: this.creds.username, // the data that will be sent to the server
+      name: this.creds.name
+    }
+    
+    let loading = this.loadingCtrl.create({
+      content: 'Creating Company...'
+    });
+    
+    loading.present();
+    
+    this.http.post('/createCompany', data).subscribe(response => {
+      var resBody = JSON.parse(response["_body"]);
+      loading.dismiss();
+      
+      if (!resBody.err) {
+        let toast = this.toastCtrl.create({
+          message: "Successfully Created Company!",
+          duration: 2000
+        });
+        
+        toast.present();
+  
+        this.navCtrl.pop(); // Remove this page from nav stack
+      } else {
+        let toast = this.toastCtrl.create({
+          message: resBody.type,
+          duration: 2000
+        });
 
+        toast.present();
+      }
+    });
   }
 }
