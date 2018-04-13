e2684aea9713616dddfd2dd12e4e68aa46b589e6
dorianSTEM <31543262+dorianSTEM@users.noreply.github.com>
2018-04-13 08:53:44 -0600

    Add files via upload

--- client/src/pages/home/home.ts
+++ client/src/pages/home/home.ts
@@ -1,14 +1,30 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { Storage } from '@ionic/storage';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  data:any = {
+    username: "",
+    company: ""
+  };
+  constructor(public navCtrl: NavController, public storage: Storage) {
+    var username;
+    var companyName;
 
-  constructor(public navCtrl: NavController) {
+    this.storage.get('username').then((val) => {
+      username = val;
+    });
 
+    this.storage.get('company').then((val) => {
+      companyName = val;
+    });
+
+    this.data.username = username;
+    this.data.company = companyName;
   }
-
 }
