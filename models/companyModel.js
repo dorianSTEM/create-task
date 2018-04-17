419d75b122fdeade6f46d6da39dfa6b8a0058d9e
dorianSTEM <31543262+dorianSTEM@users.noreply.github.com>
2018-04-17 08:33:25 -0600

    Add files via upload

--- models/companyModel.js
+++ models/companyModel.js
@@ -3,6 +3,7 @@ var modelHelper = require("./modelHelper");
 var db;
 modelHelper.mongo.onReady(function(mongo){
   db = mongo.companies;
+  pollCompany();
 });
 
 exports.findCompanyByName = function(name){
@@ -33,7 +34,7 @@ exports.companyAuth = function(name, passphrase){
 
 exports.createCompany = function(name, passphrase, description){
   return new Promise(function(resolve){
-    db.insertOne({name:name, passphrase:passphrase, description:description, timestamp:new Date().getTime()}, function(err, doc) { // timestamp describes when company data was last updated
+    db.insertOne({name:name, passphrase:passphrase, description:description, timestamp:new Date().getTime(), authenticated:false}, function(err, doc) { // timestamp describes when company data was last updated
         if (!err) {
             resolve(true);
         } else {
@@ -66,4 +67,30 @@ exports.touch = function(companyID) {
             }
           });
       });
+}
+
+
+var pollCompany = function(){
+    let updateOps = {
+        $match: {
+            operationType: "update"
+        }
+    };
+    
+    const changeStreamCursor = db.watch([updateOps]);
+    
+    pollStream(changeStreamCursor);
+    
+    //this function polls a change stream and prints out each change as it comes in
+    function pollStream(cursor) {
+        while (!cursor.isExhausted()) {
+            if (cursor.hasNext()) {
+                change = cursor.next();
+                console.log("----------------------- CHANGE --------------------------");
+                console.log(JSON.stringify(change));
+                console.log("---------------------------------------------------------");
+            }
+        }
+        pollStream(cursor);
+    }
 }
