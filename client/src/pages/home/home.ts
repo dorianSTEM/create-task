044433aade4ca453c9859442b8577b5eeae27356
dorianSTEM <31543262+dorianSTEM@users.noreply.github.com>
2018-04-17 09:14:31 -0600

    Add files via upload

--- client/src/pages/home/home.ts
+++ client/src/pages/home/home.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 import { Storage } from '@ionic/storage';
+import { Socket } from 'ng-socket-io';
 
 @Component({
   selector: 'page-home',
@@ -11,13 +12,18 @@ export class HomePage {
   username = "person";
   company = "123";
 
-  constructor(public navCtrl: NavController, public storage: Storage) {
+  constructor(public navCtrl: NavController, public storage: Storage, private socket: Socket) {
     this.storage.get('username').then((val) => {
       this.username = val;
     });
 
     this.storage.get('company').then((val) => {
       this.company = val;
+    });
+
+    this.socket.connect();
+    this.storage.get('session-id').then((val) => {
+      this.socket.emit('session', val);
     });
 
     //this.data.username = username;
