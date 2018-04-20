bd811a69b98735d5d0eae31f65ce6ee7ad26fb6c
dorianSTEM <31543262+dorianSTEM@users.noreply.github.com>
2018-04-20 12:53:39 -0600

    Add files via upload

--- sockethandler.js
+++ sockethandler.js
@@ -14,14 +14,17 @@ exports.connection = function(socket){
     });
 
     socket.on("session-id", function(sessionID){
+        console.log("Session ID acquired");
         userModel.getUserBySession(sessionID).then(function(obj){
             console.log(obj.doc);
             if (obj.found){ // if we found the user by his session, send the OK (and user info)
               compModel.findCompanyByObjID(obj.doc.companyID).then(function(compObj){
                 if (compObj.found){
                   if (!sockets[obj.doc.companyID]){
+                    console.log("ADDING TO LIST");
                     sockets[obj.doc.companyID] = [{username:obj.doc.username, socket:socket, lastCheck:0}]; // Add this socket to the list of sockets
                   } else {
+                    console.log("ADDING TO ARR");
                     sockets[obj.doc.companyID].push({username:obj.doc.username, socket:socket, lastCheck:0})
                   }
                 }
@@ -33,6 +36,7 @@ exports.connection = function(socket){
 
 exports.triggerCompany = function(company){
   console.log("COMPANY triggered");
+  console.log(JSON.stringify(sockets));
   console.log("AVAILABLE COMPANIES", sockets[company]);
   for (var sock in sockets[company]){
     eventModel.getCompanyEvents(company, sockets[company][sock].lastCheck).then(function(obj) {
