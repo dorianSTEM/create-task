c8239d01ff3df2321a880dd43208154cacb59343
dorianSTEM <31543262+dorianSTEM@users.noreply.github.com>
2018-04-20 14:33:30 -0600

    Add files via upload

--- client/src/pages/home/home.ts
+++ client/src/pages/home/home.ts
@@ -14,6 +14,9 @@ import { CreateEventPage } from "../create-event/create-event"
 export class HomePage {
   username = "person";
   company = "123";
+  timestamp: Number = 0;
+
+  eventTemplate = [];
 
   constructor(public navCtrl: NavController, public storage: Storage, private socket: Socket, public modalCtrl: ModalController) {
     this.storage.get('username').then((val) => {
@@ -27,12 +30,15 @@ export class HomePage {
     this.socket.connect();
 
     this.storage.get('session-id').then((val) => {
-      this.socket.emit('session-id', val);
+      this.socket.emit('session-id', {sessionID:val, timestamp:this.timestamp});
     });
 
     this.socket.on('new', function(info){
       console.log("-------INFO-------");
       console.log(info);
+      this.timestamp = info.timestamp; // update the timestamp based on new data
+      this.eventTemplate = this.eventTemplate.concat(info.docs);
+
       console.log("------------------");
     });
 
