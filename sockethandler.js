f51f820b1ece3afaf3b1ffff8d59a77840e6b5cc
dorianSTEM <31543262+dorianSTEM@users.noreply.github.com>
2018-04-21 18:00:52 -0600

    Add files via upload

--- sockethandler.js
+++ sockethandler.js
@@ -6,70 +6,37 @@ var sockets = {};
 //var socketCompanies = {};
 
 exports.connection = function(socket){
-    console.log('a user connected');
+  console.log('a user connected');
 
-    socket.on('disconnect', function(){
-        console.log("disconnect.");
-        for (var sock in sockets) {
-          if (sockets.hasOwnProperty(sock)) {
-            var newSock = sockets[sock]
-            for (var s in newSock){
-              if (newSock[s].socket == socket){
-                sockets[sock].splice(s, 1);
-              }
-            }
+  socket.on('disconnect', function(){
+    console.log("disconnect.");
+  });
+
+  socket.on("session-id", function(obj){ // set session ID and also send new event data
+    console.log("Session ID acquired");
+    var sessionID = obj.sessionID;
+    var timestamp = obj.timestamp || 0;
+
+    userModel.getUserBySession(sessionID).then(function(obj){
+        console.log(obj.doc);
+        eventModel.getCompanyEvents(obj.doc.companyID, timestamp).then(function(eventObj) {
+          if (obj.found){
+            socket.join(obj.doc.companyID);
+            socket.emit('new', {docs:eventObj.docs, timestamp:lastCheck});
           }
-      }
-    });
-
-    socket.on("session-id", function(obj){ // set session ID and also send new event data
-        console.log("Session ID acquired");
-        var sessionID = obj.sessionID;
-        var timestamp = obj.timestamp || 0;
-
-        userModel.getUserBySession(sessionID).then(function(obj){
-            console.log(obj.doc);
-            if (obj.found){ // if we found the user by his session, send the OK (and user info)
-              compModel.findCompanyByObjID(obj.doc.companyID).then(function(compObj){
-                var lastCheck = new Date().getTime();
-
-                if (compObj.found){
-                  if (!sockets[obj.doc.companyID]){
-                    console.log("ADDING TO LIST");
-                    sockets[obj.doc.companyID] = [{username:obj.doc.username, socket:socket, lastCheck:lastCheck}]; // Add this socket to the list of sockets
-                  } else {
-                    console.log("ADDING TO ARR");
-                    sockets[obj.doc.companyID].push({username:obj.doc.username, socket:socket, lastCheck:lastCheck})
-                  }
-                }
-                
-                console.log("WE HAVE A TIMESTAMP", timestamp);
-
-                eventModel.getCompanyEvents(obj.doc.companyID, timestamp).then(function(eventObj) {
-                  if (obj.found){
-                    for (var sock in sockets[obj.doc.companyID]){
-                      sockets[obj.doc.companyID][sock].socket.emit('new', {docs:eventObj.docs, timestamp:lastCheck});
-                    }
-                  }
-                });
-
-              });
-            }
         });
     });
+  });
 }
 
 exports.triggerCompany = function(company, timestamp){
   console.log("COMPANY triggered");
   //console.log(JSON.stringify(sockets));
-  console.log("AVAILABLE COMPANIES", sockets[company]);
-  for (var sock in sockets[company]){
-    console.log("iterating over", sock);
-    eventModel.getCompanyEvents(company, sockets[company][sock].lastCheck).then(function(obj) {
-      if (obj.found){
-        sockets[company][sock].socket.emit('new', {docs:obj.docs, timestamp:timestamp});
-        sockets[company][sock].lastCheck = timestamp;
-      }
-    });
-  }
+  //console.log("AVAILABLE COMPANIES", sockets[company]);
+  eventModel.getCompanyEvents(company, 0).then(function(obj) {
+    if (obj.found){
+      //sockets[company][sock].socket.emit('new', {docs:obj.docs, timestamp:timestamp});
+      io.sockets.in(company).emit('new', {docs:obj.docs, timestamp:timestamp});
+    }
+  });
 }
