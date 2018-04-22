11269f782671e660cfa292d53986857f0714036b
dorianSTEM <31543262+dorianSTEM@users.noreply.github.com>
2018-04-21 18:19:59 -0600

    Add files via upload

--- sockethandler.js
+++ sockethandler.js
@@ -31,7 +31,8 @@ exports.connection = function(socket){
 }
 
 exports.setIO = function(io){
-  io = io;
+  console.log("SETTING IO");
+  sockets.io = io;
 }
 
 exports.triggerCompany = function(company, timestamp){
@@ -41,7 +42,7 @@ exports.triggerCompany = function(company, timestamp){
   eventModel.getCompanyEvents(company, 0).then(function(obj) {
     if (obj.found){
       //sockets[company][sock].socket.emit('new', {docs:obj.docs, timestamp:timestamp});
-      io.sockets.in(company).emit('new', {docs:obj.docs, timestamp:timestamp});
+      sockets.io.sockets.in(company).emit('new', {docs:obj.docs, timestamp:timestamp});
     }
   });
 }
